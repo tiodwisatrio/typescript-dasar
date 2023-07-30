e8525ba46b4ee6936a0e35286b50d8f5463da721
tiodwisatrio <tiodwisatrio27@gmail.com>
2023-07-30 23:46:42 +0700

    perulangan for loop'

---
+++ tests/for-loop.test.ts
@@ -0,0 +1,20 @@
+describe("For Loop", () => {
+    it("Should support for loop", () => {
+        const names: string[] = ["John", "Jane", "Peter", "Michael", "Smith"]
+
+        // Perulangan For i
+        for (let i = 0; i < names.length; i++) {
+            console.log(names[i]);
+        }
+
+        // Perulangan for of
+        for (const name of names) {
+            console.log(name);
+        }
+
+        // Perulangan for in
+        for (const index in names) {
+            console.log(names[index]);
+        }
+    })
+})
