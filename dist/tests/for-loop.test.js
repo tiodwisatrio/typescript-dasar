d84ee5c8bd4f2dbbcf5f190e69ea5d6e5cf1e10a
tiodwisatrio <tiodwisatrio27@gmail.com>
2023-07-30 23:50:25 +0700

    Do While Loop

--- dist/tests/for-loop.test.js
+++ dist/tests/for-loop.test.js
@@ -1,37 +1,32 @@
+"use strict";
 describe("For Loop", () => {
     it("Should support for loop", () => {
-        const names: string[] = ["John", "Jane", "Peter", "Michael", "Smith"]
-
+        const names = ["John", "Jane", "Peter", "Michael", "Smith"];
         // Perulangan For i
         for (let i = 0; i < names.length; i++) {
             console.log(names[i]);
         }
-
         // Perulangan for of
         for (const name of names) {
             console.log(name);
         }
-
         // Perulangan for in
         for (const index in names) {
             console.log(names[index]);
         }
-    })
-
+    });
     it("Should support while loop", () => {
-        let counter: number = 0
-        while(counter < 10) {
+        let counter = 0;
+        while (counter < 10) {
             console.log(counter);
-            counter++
+            counter++;
         }
-    })
-
+    });
     it("Should support Do While Loop", () => {
-        let counter: number = 0;
+        let counter = 0;
         do {
-            counter++
-            console.log(counter)
-        } while (counter < 10) 
-        
-    })
-})
+            counter++;
+            console.log(counter);
+        } while (counter < 10);
+    });
+});
